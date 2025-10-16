42419de947a5f237535107a822f1e32e5c143198
rakeshcontrolcore <rakesh@controlcore.io>
2025-10-15 23:09:03 -0400

    chore: Clean up temporary documentation files
    Remove all temporary documentation and plan files

--- start_api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /Users/rakeshraghu/Code\ Projects/Control_Core/control-core-012025/cc-pap-api
-source venv/bin/activate
-exec uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
-
